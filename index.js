5259e5b24985062aeace6fe23f8057bf4609a422
eymoree <karlavc.tns@gmail.com>
2024-07-24 15:53:37 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeca = document.getElementById("muñeca");
+  
+    let textoCifrado = texto
+      .replace(/a/gi, "ai")
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeca.src = "./img/muñeca.png";
+    } else {
+      muñeca.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeca = document.getElementById("muñeca");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeca.src = "./img/muñeca.png";
+      } else {
+        muñeca.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+  }
+
+  function copiarTexto() {
+    var textarea = document.getElementById("texto");
+    
+    navigator.clipboard.writeText(textarea.value)
+      .then(() => {
+        alert("Texto copiado: " + textarea.value);
+      })
+      .catch(err => {
+        console.error("Error al copiar el texto: ", err);
+      });
+  }
+  
+  function limpiarcaja() {
+    document.getElementById("texto").value = '';
+  }
